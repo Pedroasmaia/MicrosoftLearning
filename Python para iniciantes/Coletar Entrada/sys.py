04032cc53596afc3f96088ed0f061c2a54ea4937
Pedro Maia <pedro.maia@ivoryit.com.br>
2022-08-01 13:08:32 -0300

    Escreva seus primeiros programas em Python

---
+++ Python para iniciantes/Coletar Entrada/sys.py
@@ -0,0 +1,9 @@
+import sys
+
+print(f'O primeiro argumento foi {sys.argv[1]}')
+print(f'O segundo argumento foi {sys.argv[2]}')
+
+
+#"Ao rodar no CLI python sys.py pedro maria o resultado será"
+#"O primeiro argumento foi pedro"
+#"O segundo argumento foi maria"
