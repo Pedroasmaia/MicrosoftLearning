beadc3d295465ee24a4e0f31daaaed0cef5fd104
Pedro Maia <pedroasmaia@outlook.com>
2023-02-16 23:26:16 -0300

    Unidade 2

---
+++ Executar formatação de cadeia de caracteres básica em C#/Unidade 2.cs
@@ -0,0 +1,13 @@
+//Sequencias de caractés de escape
+
+// \n adiciona uma nova linha
+Console.WriteLine("Pedro\nMaia");
+// \t adiciona um espaçamento
+Console.WriteLine("Hello\tWorld!");
+// \" adiciona aspas duplas
+Console.WriteLine("Vamos dar um \"jeitinho\" para conseguir.");
+// Para inserir uma barra invertida você duplica a \\
+Console.WriteLine("Vamos dar um \"jeitinho\" para conseguir");
+
+//Caracteres Unicode
+Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
